572f546899744e776a6a0bcc0c7a6448a1148eae
da4nick07 <vlevkovsky@mail.ru>
2023-02-06 19:02:12 +0300

    Начал test5

---
+++ test5/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to example.com</title>
+</head>
+<body>
+<h1>Success!</h1>
+<h1>It's virtual host!</h1>
+</body>
+</html>
